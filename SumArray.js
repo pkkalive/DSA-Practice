0ec122fcb85d0ee54241f35b954744383c7a29f0
pkkalive <kalivepurushothamkumar@gmail.com>
2022-08-27 17:55:20 +0530

    sumArray

---
+++ SumArray.js
@@ -0,0 +1,13 @@
+const arr = [34, 57, 82, 41, 65, 35, 82, 27, 36, 12, 6, 40, 66, 99, 25, 29, 22, 25, 12, 24, 65, 15, 5, 43, 28, 33, 76, 32, 13, 95, 22, 84, 71, 23, 28, 7, 5, 94, 18, 47, 9, 42, 61, 73]
+
+function sumArray(A){
+    let sum = 0;
+    if (A.length === 0){
+        return sum;
+    }
+    sum += A[A.length -1]
+    A.length--
+    return sum + sumArray(A);
+}
+
+console.log(sumArray(arr))
